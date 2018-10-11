32bc37852e6d90c53ed306ae950739cacf8f8c01
gelosofederico <gelosofederico@gmail.com>
2018-10-11 00:16:14 -0300

    Agregado DyC y arreglado algunas cosas del print

--- ConvexHull.py
+++ ConvexHull.py
@@ -14,7 +14,8 @@ def main(points_file,M):
     ## Interpreto el archivo
     # En la posición 0 de cada punto está la coordenada X, en la 1 es la Y
     points = read_points(points_file)
-    M = "G"
+    print(points)
+    M = "D"
     if M == "F":
         convex_hull = convex_hull_brute(points)
     if M == "G":
@@ -22,6 +23,7 @@ def main(points_file,M):
     if M == "D":
         convex_hull = convex_hull_DC(points)
     ## Acá hay que sacar que lado conviene e imprimirlo como pide el enunciado
+    print(points)
     print_path(points,convex_hull)
 
     return 
@@ -102,7 +104,7 @@ def print_path(points,hull):
 
     st_i =hull.index(start)
     last = hull[st_i]
-    st_i = st_i+1
+    st_i = (st_i+1)%len(hull)
     distance = 0
     path = [last]
     while hull[st_i]!=finish:
@@ -138,6 +140,122 @@ def print_path(points,hull):
 def distance_between(a,b):
     return math.sqrt((b[0]-a[0])**2+(b[1]-a[1])**2)
 
+def convex_hull_DC(points):
+    # Se ordena según x, y se va dividiendo a partes iguales.
+    # Se llama recursivamente para cada mitad y de cada uno devuelve el CH,
+    # ordenados de forma antihoraria, empezando con el punto de menor x
+    p = points[:]
+    p.sort(key=lambda x:x[0])
+    return _convex_hull_DC(p)
+
+def _convex_hull_DC(points):
+    if len(points) <= 2:
+        return points
+    points_left = points[0:len(points)//2]
+    CH_L=_convex_hull_DC(points_left)
+    points_right = points[len(points)//2:]
+    CH_R=_convex_hull_DC(points_right)
+    return _CH_merge(CH_L,CH_R)
+
+def _CH_merge(CHL,CHR):
+    # Los convex hull se suponen en sentido antihorario ambos, empezando por el
+    # punto en el extremo izquierdo
+    # Para unirlos, se verá cual es la tangente entre ambos polígonos, y se
+    # descartaran los que están en el medio.
+    # Para hallar los puntos que forman la tangente, se recorre tomando un punto
+    # de cada lado y se busca cual pareja toca la linea vertical media más alto
+    # y más bajo
+
+    # Busco el punto de mayor coordenada X en el de la izquierda
+    if len(CHL)==1 and len(CHR)==1:
+        return CHL+ CHR
+    maxX =CHL[0][0]
+    start_CHL = 0
+    for i in range(0,len(CHL)):
+        if maxX < CHL[i][0]:
+            maxX = CHL[i][0]
+            start_CHL = i
+
+    # Siempre empiezo a recorrer el de la izquierda por start_CHL
+    # En middle está el punto medio entre los cascos
+    middle = (CHL[start_CHL][0]+CHR[0][0])/2
+
+    # Saco tangente superior
+    # l y r son el punto en el que estoy parado en cada casco
+    # l y r next son los siguientes, según su movimiento(antihorario y horario)
+    # y_m es la intersección con el medio actual
+    # y_r e y_l son las que ocurren con l o r next
+    # Se busca que y_m sea maxima, que ocurre cuando y_r e y_l son menores 
+    # ambas
+    l = start_CHL
+    r = 0
+    l_next = (l+1)%len(CHL)
+    r_next = (r-1)%len(CHR)
+    y_m = intersect(CHL[l],CHR[r],middle)
+    y_l = intersect(CHL[l_next],CHR[r],middle)
+    y_r = intersect(CHL[l],CHR[r_next],middle)
+    while (y_m<y_l) or (y_m<y_r):
+        if y_m<y_l:
+            y_m = y_l
+            l = l_next
+            l_next = (l+1)%len(CHL)
+            y_l = intersect(CHL[l_next],CHR[r],middle)
+            y_r = intersect(CHL[l],CHR[r_next],middle)
+        elif y_m<y_r:
+            y_m = y_r
+            r =r_next
+            r_next = (r-1)%len(CHR)
+            y_r = intersect(CHL[l],CHR[r_next],middle)
+            y_l = intersect(CHL[l_next],CHR[r],middle)
+    upper_tangent = [l,r]
+
+    # Tangente inferior
+    l = start_CHL
+    r = 0
+    l_next = (l+1)%len(CHL)
+    r_next = (r+1)%len(CHR)
+    y_m = intersect(CHL[l],CHR[r],middle)
+    y_l = intersect(CHL[l_next],CHR[r],middle)
+    y_r = intersect(CHL[l],CHR[r_next],middle)
+    while (y_m>y_l) or (y_m>y_r):
+        if y_m>y_l:
+            y_m = y_l
+            l = l_next
+            l_next = (l+1)%len(CHL)
+            y_l = intersect(CHL[l_next],CHR[r],middle)
+        elif y_m>y_r:
+            y_m = y_r
+            r =r_next
+            r_next = (r+1)%len(CHR)
+            y_r = intersect(CHL[l],CHR[r_next],middle)
+    lower_tangent = [l,r]
+
+    # Uno en orden antihorario
+    CH = []
+    i=0
+    while i != lower_tangent[0]:
+        CH.append(CHL[i])
+        i +=1
+    CH.append(CHL[i])
+    j = lower_tangent[1]
+    while j != upper_tangent[1]:
+        CH.append(CHR[j])
+        j = (j+1)%len(CHR)
+    CH.append(CHR[j])
+    # Así no lleno de más
+    if upper_tangent[0] > i:
+        i = upper_tangent[0]
+        while i != len(CHL) and i != lower_tangent[0] :
+            CH.append(CHL[i])
+            i +=1
+    return CH
+
+def intersect(a,b,x):
+    # Da el valor de y de la intersección de la recta definida por a,b a la
+    # coordenada x 
+    # Se asume que no están en una linea vertical a y b
+    m = (b[1]-a[1])/(b[0]-a[0])
+    return m*(x-a[0])+a[1]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='parser.')
